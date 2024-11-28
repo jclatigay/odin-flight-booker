4ec0f6f0b962854805343450db66b429528726ae
John Isaac Latigay <jclatigay@up.edu.ph>
2024-11-28 19:30:57 +0800

    airport and flight model

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,51 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Airport.create(code: "SFO")
+Airport.create(code: "NYC")
+Airport.create(code: "LAX")
+Airport.create(code: "ATL")
+
+Flight.create(
+  departure_airport_id: Airport.find_by(code: "SFO").id,
+  arrival_airport_id: Airport.find_by(code: "NYC").id,
+  start: "2024-11-25 08:00:00",
+  duration: 360
+)
+Flight.create(
+  departure_airport_id: Airport.find_by(code: "NYC").id,
+  arrival_airport_id: Airport.find_by(code: "LAX").id,
+  start: "2024-11-26 10:00:00",
+  duration: 400
+)
+Flight.create(
+  departure_airport_id: Airport.find_by(code: "ATL").id,
+  arrival_airport_id: Airport.find_by(code: "SFO").id,
+  start: "2024-11-27 14:00:00",
+  duration: 300
+)
+Flight.create(
+  departure_airport_id: Airport.find_by(code: "LAX").id,
+  arrival_airport_id: Airport.find_by(code: "ATL").id,
+  start: "2024-11-28 16:00:00",
+  duration: 420
+)
+Flight.create(
+  departure_airport_id: Airport.find_by(code: "SFO").id,
+  arrival_airport_id: Airport.find_by(code: "NYC").id,
+  start: "2024-11-29 18:00:00",
+  duration: 360
+)
+Flight.create(
+  departure_airport_id: Airport.find_by(code: "NYC").id,
+  arrival_airport_id: Airport.find_by(code: "LAX").id,
+  start: "2024-11-30 20:00:00",
+  duration: 400
+)
+Flight.create(
+  departure_airport_id: Airport.find_by(code: "ATL").id,
+  arrival_airport_id: Airport.find_by(code: "SFO").id,
+  start: "2024-12-01 22:00:00",
+  duration: 300
+)
